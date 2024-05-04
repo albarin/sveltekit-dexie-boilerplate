ae5640023431916682171eb77139f15611a3e34e
Alba Rincón <alba@rincon.me>
2024-05-04 09:17:17 +0200

    Extract components

---
+++ src/lib/components/common/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Account from './Account.svelte';
+	import Logo from './Logo.svelte';
+</script>
+
+<header class="sticky top-0 flex justify-between h-16 items-center">
+	<Logo />
+	<Account />
+</header>
---
+++ src/lib/components/common/Logo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Snail } from 'lucide-svelte';
+</script>
+
+<a href="/" class="flex gap-1 items-center">
+	<Button size="icon" variant="link">
+		<Snail class="h-7 w-7" strokeWidth="1.8" />
+	</Button>
+	<h1 class="text-xl">Home</h1>
+</a>
