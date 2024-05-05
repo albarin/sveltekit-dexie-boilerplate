3080badaac08d349aa1ac62935c58824d86b26d5
Alba Rincón <alba@rincon.me>
2024-05-05 20:08:22 +0200

    Add account logged in/out menu

--- src/lib/components/common/Account.svelte
+++ src/lib/components/common/Account.svelte
@@ -1,24 +1,12 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button/index.js';
-	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-	import CircleUserRound from 'lucide-svelte/icons/circle-user-round';
-	import Logo from './Logo.svelte';
+	import LoggedInAccount from './LoggedInAccount.svelte';
+	import LoggedOutAccount from './LoggedOutAccount.svelte';
+
+	const loggedIn = true;
 </script>
 
-<DropdownMenu.Root>
-		<DropdownMenu.Trigger asChild let:builder>
-			<Button builders={[builder]} variant="link">
-				<CircleUserRound class="h-7 w-7" />
-				<span class="sr-only">Toggle user menu</span>
-			</Button>
-		</DropdownMenu.Trigger>
-		<DropdownMenu.Content align="end">
-			<DropdownMenu.Label>My Account</DropdownMenu.Label>
-			<DropdownMenu.Separator />
-			<DropdownMenu.Item>Settings</DropdownMenu.Item>
-			<DropdownMenu.Item>Support</DropdownMenu.Item>
-			<DropdownMenu.Separator />
-			<DropdownMenu.Item>Logout</DropdownMenu.Item>
-		</DropdownMenu.Content>
-	</DropdownMenu.Root>
-
+{#if loggedIn}
+	<LoggedInAccount />
+{:else}
+	<LoggedOutAccount />
+{/if}
---
+++ src/lib/components/common/LoggedOutAccount.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<a href="/">Sign in</a>
