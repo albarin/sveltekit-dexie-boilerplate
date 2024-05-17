599ae51707f7edfdab3e09754deeeffd62bd7335
Alba Rincón <alba@rincon.me>
2024-05-17 18:52:26 +0200

    Improve logo structure

--- src/lib/components/common/header/Logo.svelte
+++ src/lib/components/common/header/Logo.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button/index.js';
 	import { Snail } from 'lucide-svelte';
 </script>
 
 <div class="flex flex-1 justify-start gap-1 items-center">
-	<Button size="icon" variant="link" href="/">
+	<a class="flex gap-2" href="/">
 		<Snail class="h-8 w-8" />
-	</Button>
-	<a href="/">
 		<h1 class="text-xl">Logo</h1>
 	</a>
 </div>
