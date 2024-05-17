dc814bf8eecfa6ce926d8c153fd1ad1fc6c55f2e
Alba Rincón <alba@rincon.me>
2024-05-17 07:44:47 +0200

    Add theme switcher title

--- src/lib/components/common/header/ThemeSwitcher.svelte
+++ src/lib/components/common/header/ThemeSwitcher.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { Moon, Sun } from 'lucide-svelte';
-	import { toggleMode } from 'mode-watcher';
+	import { toggleMode, mode } from 'mode-watcher';
+
+	const modeTitle = $derived($mode === 'dark' ? 'Switch to light mode' : 'Switch to dark mode');
 </script>
 
-<Button on:click={toggleMode} variant="ghost" size="icon">
+<Button on:click={toggleMode} variant="ghost" size="icon" title={modeTitle}>
 	<Sun class="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 	<Moon class="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 	<span class="sr-only">Toggle theme</span>
