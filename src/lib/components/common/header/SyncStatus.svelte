4598e32cf559b3f15b9a3cf2e4b515809ba90518
Alba Rincón <alba@rincon.me>
2024-05-21 21:52:30 +0200

    Add sync status translation

--- src/lib/components/common/header/SyncStatus.svelte
+++ src/lib/components/common/header/SyncStatus.svelte
@@ -2,6 +2,7 @@
 	import { inSync } from '$lib/auth';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { db } from '$lib/db';
+	import { t } from '$lib/translations';
 	import { RefreshCw } from 'lucide-svelte';
 
 	const syncState = db.cloud.syncState;
@@ -9,7 +10,7 @@
 </script>
 
 {#if !synced}
-	<Button size="icon" variant="ghost">
+	<Button size="icon" variant="ghost" title={$t('header.sync.syncing')}>
 		<RefreshCw class="animate-spin-slow" />
 	</Button>
 {/if}
