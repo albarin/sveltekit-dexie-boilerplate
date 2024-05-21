02ac5c431cc87f9849e9f9c2e5ac0caeb4c78858
Alba Rincón <alba@rincon.me>
2024-05-21 15:45:49 +0200

    Add favicons and warning if offline

--- src/lib/components/common/header/Header.svelte
+++ src/lib/components/common/header/Header.svelte
@@ -5,7 +5,12 @@
 	import SyncStatus from './SyncStatus.svelte';
 	import ThemeSwitcher from './ThemeSwitcher.svelte';
 	import Notifications from './notifications/BellButton.svelte';
+	import { browser } from '$app/environment';
+
+	let isOnline = $state(browser ? navigator.onLine : true);
 </script>
+
+<svelte:window on:online={() => (isOnline = true)} on:offline={() => (isOnline = false)} />
 
 <header class="sticky top-0 bg-background border-b py-1 px-4">
 	<div class="flex items-center">
@@ -21,3 +26,10 @@
 		</div>
 	</div>
 </header>
+
+{#if !isOnline}
+	<div class="sticky top-0 bg-red-500 text-white text-sm text-center border-b py-1 px-4">
+		No network connection. Some features may be unavailable. Sync will resume when connection is
+		restored.
+	</div>
+{/if}
