d0a151d774edaa64d54dfe52a871434e3d1b88e0
Alba Rincón <alba@rincon.me>
2024-06-02 19:53:47 +0200

    Delete expired suscriptions after failure

--- src/routes/api/notify/+server.ts
+++ src/routes/api/notify/+server.ts
@@ -1,6 +1,6 @@
 import { CONTACT_EMAIL, VAPID_PRIVATE_KEY, VAPID_PUBLIC_KEY } from '$env/static/private';
 import dexie from '$lib/dexie';
-import webpush from 'web-push';
+import webpush, { WebPushError } from 'web-push';
 
 webpush.setVapidDetails(`mailto:${CONTACT_EMAIL}`, VAPID_PUBLIC_KEY, VAPID_PRIVATE_KEY);
 
@@ -8,7 +8,16 @@ export const POST = async () => {
   const subscriptions = await dexie.getAllSubscriptions();
 
   for (const subscription of subscriptions) {
-    await webpush.sendNotification(subscription.subscription, 'Hello, world!');
+    try {
+      await webpush.sendNotification(subscription.subscription, 'Hello, world!');
+
+    } catch (err) {
+      console.error('Failed to send notification:', err);
+      if ((err as WebPushError).statusCode === 410) {
+        console.error(`Subscription expired, deleting subscription: ${subscription.id}`);
+        await dexie.deleteSubscription(subscription.id);
+      }
+    }
   }
 
   return new Response();
