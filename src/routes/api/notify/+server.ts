179a0caeedc320d2b61d399eb38f6b015b1cdc7e
Alba Rincón <alba@rincon.me>
2024-06-02 17:45:30 +0200

    Fix notify endpoint to sent notifications to all subscribed

--- src/routes/api/notify/+server.ts
+++ src/routes/api/notify/+server.ts
@@ -5,7 +5,7 @@ import webpush from 'web-push';
 webpush.setVapidDetails(`mailto:${CONTACT_EMAIL}`, VAPID_PUBLIC_KEY, VAPID_PRIVATE_KEY);
 
 export const POST = async () => {
-  const subscriptions = await dexie.getSubscriptions('alba@rincon.me');
+  const subscriptions = await dexie.getAllSubscriptions();
 
   for (const subscription of subscriptions) {
     await webpush.sendNotification(subscription.subscription, 'Hello, world!');
