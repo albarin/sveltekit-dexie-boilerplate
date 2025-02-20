104aeecded2208eb45314109a13cf7e61a64f4bf
Alba Rincón <alba@rincon.me>
2025-02-20 21:07:57 +0100

    Fix button pointer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
 	import { Notification } from '$lib/db';
 
 	async function test() {
@@ -7,6 +8,7 @@
 </script>
 
 <h1>Welcome to SvelteKit</h1>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<button onclick={test}>test</button>
+<Button onclick={test}>test</Button>
