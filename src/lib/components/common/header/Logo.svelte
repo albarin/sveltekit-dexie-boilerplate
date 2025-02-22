eace1b7becdf85cac56fc48716d62c294b3c4201
Alba Rincón <alba@rincon.me>
2025-02-22 17:14:13 +0100

    Add title from the env

--- src/lib/components/common/header/Logo.svelte
+++ src/lib/components/common/header/Logo.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import { PUBLIC_VITE_APP_NAME } from '$env/static/public';
 	import { Snail } from 'lucide-svelte';
 </script>
 
 <div class="flex flex-1 justify-start gap-1 items-center">
 	<a class="flex gap-2 items-center" href="/">
 		<Snail class="h-8 w-8" />
-		<h1 class="text-xl">Logo</h1>
+		<h1 class="text-xl">{PUBLIC_VITE_APP_NAME}</h1>
 	</a>
 </div>
