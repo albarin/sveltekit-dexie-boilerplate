3a344351a0bf76b02c7cc2b8204ae9085654ad39
Alba Rincón <alba@rincon.me>
2025-03-08 12:17:15 +0100

    Add submit on enter component

---
+++ src/lib/components/SubmitOnEnter.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let { action } = $props();
+
+	const submitOnEnter = (e: KeyboardEvent) => {
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			action(e);
+		}
+	};
+</script>
+
+<svelte:window onkeydown={submitOnEnter} />
